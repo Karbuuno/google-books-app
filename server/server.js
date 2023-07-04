eaf5c252412d10f03539f4fa1cd2f65f38e6b8b1
Abdishakur Abdalla Gesey <karbuuno@gmail.com>
2023-07-04 22:48:17 +0100

    fix: refactor hard coded connection string to Heroku config var

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const dotenv = require("dotenv");
 // import ApolloServer
 const { ApolloServer } = require("apollo-server-express");
 const { authMiddleware } = require("./utils/auth");
@@ -6,7 +7,7 @@ const { authMiddleware } = require("./utils/auth");
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
-
+dotenv.config();
 const PORT = process.env.PORT || 3001;
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
